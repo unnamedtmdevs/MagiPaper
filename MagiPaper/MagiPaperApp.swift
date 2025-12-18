24b5a3ed029a72ba222aca1fc387bf695972cf93
ludik <bakhanetssimon@gmail.com>
2025-12-18 23:12:49 +0700

    Initial Commit

---
+++ MagiPaper/MagiPaperApp.swift
@@ -0,0 +1,17 @@
+//
+//  MagiPaperApp.swift
+//  MagiPaper
+//
+//  Created by Simon Bakhanets on 18.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct MagiPaperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MagiPaperTests/MagiPaperTests.swift
@@ -0,0 +1,16 @@
+//
+//  MagiPaperTests.swift
+//  MagiPaperTests
+//
+//  Created by Simon Bakhanets on 18.12.2025.
+//
+
+import Testing
+
+struct MagiPaperTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
